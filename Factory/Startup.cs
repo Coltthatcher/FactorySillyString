d54bed64fba2d3b39b72677b6f6c89d77833f37d
Colt Thatcher <colt.thatcher303@gmail.com>
2022-01-07 11:43:29 -0800

    add boilerplate for DesignContext

--- Factory/Startup.cs
+++ Factory/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Factory.Models;
 
-namespace RestaurantsCuisines
+namespace Factory
 {
   public class Startup
   {
