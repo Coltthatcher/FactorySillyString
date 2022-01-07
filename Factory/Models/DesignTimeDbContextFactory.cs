ee05b7eac9e365e3d502d845f23a56ab4928bbcc
Colt Thatcher <colt.thatcher303@gmail.com>
2022-01-07 11:45:38 -0800

    Changed namespace in design context

--- Factory/Models/DesignTimeDbContextFactory.cs
+++ Factory/Models/DesignTimeDbContextFactory.cs
@@ -3,23 +3,23 @@ using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 
-namespace RestaurantsCuisines.Models
+namespace Factory.Models
 {
-  public class RestaurantCuisineContextFactory : IDesignTimeDbContextFactory<RestaurantCuisineContext>
+  public class FactoryContextFactory : IDesignTimeDbContextFactory<FactoryContext>
   {
 
-    RestaurantCuisineContext IDesignTimeDbContextFactory<RestaurantCuisineContext>.CreateDbContext(string[] args)
+    FactoryContext IDesignTimeDbContextFactory<FactoryContext>.CreateDbContext(string[] args)
     {
       IConfigurationRoot configuration = new ConfigurationBuilder()
           .SetBasePath(Directory.GetCurrentDirectory())
           .AddJsonFile("appsettings.json")
           .Build();
 
-      var builder = new DbContextOptionsBuilder<RestaurantCuisineContext>();
+      var builder = new DbContextOptionsBuilder<FactoryContext>();
 
       builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
 
-      return new RestaurantCuisineContext(builder.Options);
+      return new FactoryContext(builder.Options);
     }
   }
 }
